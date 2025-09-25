6279429b14fc3cffa68832cf4c42458ba8b6a9e1
Sebastian Beltran <juan.beltran0518@hotmail.com>
2025-09-25 12:13:58 -0500

    Initial commit: Spring Boot AREP CRUD System project setup

---
+++ src/main/java/arep/edu/co/arepcrudsystem/ArepcrudsystemApplication.java
@@ -0,0 +1,13 @@
+package arep.edu.co.arepcrudsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArepcrudsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArepcrudsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arep/edu/co/arepcrudsystem/ArepcrudsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package arep.edu.co.arepcrudsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArepcrudsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
